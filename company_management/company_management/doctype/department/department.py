fa09b8c43d2c9e94f347225c5508e45f9b041bea
loaywaleed <loaywaleed9@gmail.com>
2025-01-12 17:46:01 +0200

    Feat: controller methods to handle auto calculations for department field

--- company_management/company_management/doctype/department/department.py
+++ company_management/company_management/doctype/department/department.py
@@ -3,4 +3,18 @@ import frappe
 
 
 class Department(Document):
-    pass
+    def after_insert(self):
+        frappe.msgprint("Department Updated")
+        company = frappe.get_doc("Company", self.company)
+        company.number_of_departments = frappe.db.count(
+            "Department", {"Company": self.company}
+        )
+        company.save(ignore_permissions=True)
+
+    def on_trash(self):
+        frappe.msgprint("Department Deleted")
+        company = frappe.get_doc("Company", self.company)
+        company.number_of_departments = (
+            frappe.db.count("Department", {"Company": self.company}) - 1
+        )
+        company.save(ignore_permissions=True)
