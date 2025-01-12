222a8d759a2594cdf39323a26768b56679fa90d2
loaywaleed <loaywaleed9@gmail.com>
2025-01-12 18:30:43 +0200

    feat: auto update department number of employees

--- company_management/company_management/doctype/employee/employee.py
+++ company_management/company_management/doctype/employee/employee.py
@@ -1,3 +1,4 @@
+from company_management.company_management.doctype import department
 from frappe.model.document import Document
 import frappe
 
@@ -6,15 +7,25 @@ class Employee(Document):
     def after_insert(self):
         frappe.msgprint("Employee Updated")
         company = frappe.get_doc("Company", self.company)
+        department = frappe.get_doc("Department", self.department)
         company.number_of_employees = frappe.db.count(
             "Employee", {"Company": self.company}
         )
+        department.number_of_employees = frappe.db.count(
+            "Employee", {"Department": self.department}
+        )
+        department.save(ignore_permissions=True)
         company.save(ignore_permissions=True)
 
     def on_trash(self):
         frappe.msgprint("Employee Deleted")
         company = frappe.get_doc("Company", self.company)
+        department = frappe.get_doc("Department", self.department)
         company.number_of_employees = (
             frappe.db.count("Employee", {"Company": self.company}) - 1
         )
+        department.number_of_employees = (
+            frappe.db.count("Employee", {"Department": self.department}) - 1
+        )
+        department.save(ignore_permissions=True)
         company.save(ignore_permissions=True)
